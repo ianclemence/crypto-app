0fb0beec3b2f3187558378a1ecd4086261752650
Ian Clemence <ianclemence17@gmail.com>
2022-05-17 16:44:44 +0300

    Initialize cryptoApi

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '3f3eb66346msh0a4b6db882d8ffcp1b248ajsn8647f29403d6'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    requcerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
